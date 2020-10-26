fa80a32ff9b8297068565aea7b27b6478ba33df4
wkd3329 <psj5048@gmail.com>
2020-10-26 19:40:34 +0900

    카메라실행

--- app/src/main/java/com/example/app/frag_add.java
+++ app/src/main/java/com/example/app/frag_add.java
@@ -1,6 +1,10 @@
 package com.example.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.security.identity.CipherSuiteNotSupportedException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class frag_add extends Fragment {
+
+    private  static final int IMAGE = 672;
+    private String imagePath;
+    private Uri imgUri;
+
 
     Button btn_addpic;
     Button btn_addtxt;
@@ -31,6 +40,27 @@ public class frag_add extends Fragment {
         btn_addtxt = view.findViewById(R.id.btn_addbytxt);
         btn_addexer = view.findViewById(R.id.btn_addexer);
 
+        btn_addpic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            }
+        });
+
+        btn_addalbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 101);
+            }
+
+            });
+
+
         return view;
+
     }
 }
+
